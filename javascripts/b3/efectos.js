a3134c798c1d95b351885920e5ae476192a8cff6
Ceciram <cecilia.arel.ramirez@gmail.com>
2015-04-23 01:22:45 -0500

    correccion de la seccion farolito para estudiantes

---
+++ javascripts/b3/efectos.js
@@ -0,0 +1,9 @@
+<script>
+    $( document ).ready(function() {        
+        $(".circulo-grande").hover(function()
+        {
+            $(this).css("opacity","0");
+            $(this . ".dropdown-menu").show();
+        });        
+    });        
+</script>
